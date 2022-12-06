72a4f0635347772f93b72fc0c56c5c00e0b6e0f4
kablima-git <kalilinux@gmail.com>
2022-12-06 13:59:41 -0500

    Added extra example.

--- summing-function.py
+++ summing-function.py
@@ -4,9 +4,12 @@ def mysum(*numbers):
         sum += number
     return sum
 
-print(mysum(1,12, 25, 7))
+print(mysum(1, 12,25, 7))
 # 45
+print(mysum(3, 9, 19))
+# 31
 print(mysum(*[10, 32]))
 # 42
 print(mysum(*{8, 14, 19, 59, 101}))
 # 201
+
